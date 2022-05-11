6b88a383006358ef539166cff0d352a899a6bc4c
Bryan <bryannobug@gmail.com>
2022-05-11 23:46:37 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, styled} from 'baseui';
+import {ListItem, ListItemLabel} from "baseui/list";
+import {Checkbox} from "baseui/checkbox";
+import {useState} from "react";
+import {Card} from "baseui/card";
+
+const engine = new Styletron();
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  paddingTop: '50px'
+});
+export default function Hello() {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Centered>
+          <Card  overrides={{Root: {style: {width: '328px', height: '500px'}}}} title="Now">
+            <ListItem
+              artwork={() => <Checkbox
+              checked={checked}
+              onChange={e => setChecked(e.target.checked)}
+                />}
+            >
+              <ListItemLabel>Label</ListItemLabel>
+          </ListItem>
+          </Card>
+        </Centered>
+      </BaseProvider>
+    </StyletronProvider>
+  );
+}
