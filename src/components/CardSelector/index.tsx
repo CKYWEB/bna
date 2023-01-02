2160d2265333e94fb14755232f55b2ab0a4933d1
Bryan <bryannobug@gmail.com>
2023-01-03 00:03:50 +0800

    add: CardSelector

---
+++ src/components/CardSelector/index.tsx
@@ -0,0 +1,37 @@
+import {Select, Value} from "baseui/select";
+import {useState} from "react";
+
+const CARD_TYPES = [
+  { label: "Before", id: "Before", },
+  { label: "Now", id: "Now", },
+  { label: "After", id: "After", }
+]
+
+export default function CardSelector () {
+  const [value, setValue] = useState<Value>([CARD_TYPES[1]]);
+
+  return (
+    <Select
+      overrides={{
+        ValueContainer: {
+          style: {
+            backgroundColor: '#FFF',
+            fontSize: '18px',
+            fontWeight: 700,
+          },
+        },
+        IconsContainer: {
+          style: {
+            backgroundColor: '#FFF',
+          },
+        },
+      }}
+      clearable={false}
+      labelKey="id"
+      options={CARD_TYPES}
+      searchable={false}
+      value={value}
+      onChange={params => setValue(params.value)}
+    />
+  )
+}
